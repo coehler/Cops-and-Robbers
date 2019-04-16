096a50ee3684f021f967f83377d4020fd76f8bda
Christopher Oehler <coehler@uoguelph.ca>
2019-04-16 19:01:43 -0400

    Implemented basic TSAI functionality

---
+++ Cops-and-Robbers/Assets/C and R Scripts/FirstPersonMovementController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovementController : MonoBehaviour {
+    public float speed = 10.0F;
+
+    // Initialize controller.
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+
+    // Update is called once per frame.
+    void Update() {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyDown("escape")) {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
