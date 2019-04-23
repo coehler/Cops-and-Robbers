1127ba1cd53c7736ba0e0cd6508f05c4dc8e0c5f
Christopher Oehler <coehler@uoguelph.ca>
2019-04-22 21:52:07 -0400

    Added basic RigidBody movement to player character.

--- Cops-and-Robbers/Assets/C and R Scripts/FirstPersonMovementController.cs
+++ Cops-and-Robbers/Assets/C and R Scripts/FirstPersonMovementController.cs
@@ -3,27 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FirstPersonMovementController : MonoBehaviour {
-    public float speed = 10.0F;
+
+    private float forwardInput;
+    private float sideInput;
+
+    private Vector3 movementTarget;
+    private Rigidbody rb;
+
+    public float standingMoveSpeed = 6.0f;
+    public float sprintMoveSpeed = 10.0f;
+    public float crouchMoveSpeed = 3.0f;
+    public float sprintForwardsMult = 2.0f;
+    public float sprintSidewaysMult = 0.5f;
+
+    public KeyCode sprintKeyCode = KeyCode.LeftShift;
+    public KeyCode crouchKeyCode = KeyCode.LeftControl;
+    public KeyCode jumpKeyCode = KeyCode.Space;
 
     // Initialize controller.
     void Start() {
+
         Cursor.lockState = CursorLockMode.Locked;
+        rb = gameObject.GetComponent<Rigidbody>();
+
     }
-
-
 
     // Update is called once per frame.
-    void Update() {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float strafe = Input.GetAxis("Horizontal") * speed;
+    void FixedUpdate() {
 
-        translation *= Time.deltaTime;
-        strafe *= Time.deltaTime;
+        forwardInput = Input.GetAxis("Vertical");
+        sideInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(strafe, 0, translation);
+        // Get player state from Unity's Input API.
 
-        if (Input.GetKeyDown("escape")) {
-            Cursor.lockState = CursorLockMode.None;
+        if (Input.GetKey(crouchKeyCode)) { // The crouch key is being pressed.
+
+            MoveToTarget(forwardInput, sideInput, crouchMoveSpeed); // Move the player character crouching.
+
+        } else if (Input.GetKey(sprintKeyCode)) { // The sprint key is being pressed.
+
+            MoveToTarget(forwardInput * sprintForwardsMult, sideInput * sprintSidewaysMult, sprintMoveSpeed); // Move the player character while sprinting.
+
+        } else { // No state key is being pressed, so the player must be standing.
+
+            MoveToTarget(forwardInput, sideInput, standingMoveSpeed); // Move the player character while standing.
+
         }
     }
+
+    /*
+     * Moves the player character based to a target calculated from Input.
+     * 
+     * @param fi a float value representing forwards motion.
+     * @param si a float value representing sideways motion.
+     * @author Christopher Oehler.
+     */ 
+    void MoveToTarget(float fi, float si, float speed) {
+        
+        movementTarget = (transform.forward * fi) + (transform.right * si); // Calculate a movement target vector for given forwards and sideways inputs.
+        
+        rb.MovePosition(rb.position + movementTarget.normalized * speed * Time.deltaTime); // Apply a normalized and time adjusted target vector to.
+
+        /*
+         * 
+         * AddForce
+         * Velocity
+         * 
+         */
+
+    }
+
 }
